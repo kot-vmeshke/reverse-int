0e23f574525fd9af3352022747dfade042bc23aa
kot-vmeshke <kot-vmeshke.4@gmail.com>
2021-08-09 22:21:28 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let nStr = String(n);
+    let arr = Array.from(nStr);
+    let arrReverse = [];
+    if (arr[0] === '-') {
+        for (let i = arr.length-1; i >= 1; i--) {
+            arrReverse.push(arr[i]);
+        }
+    } else {
+        for (let i = arr.length-1; i >= 0; i--) {
+            arrReverse.push(arr[i]);
+        }
+    }
+    return Number(arrReverse.join(''));
 }
